bf6ea835647c2f97814c5aeaf10e7b8ab60a2427
samirfaritov <samirfaritov6@gmail.com>
2023-12-02 20:31:44 +0500

    itemHtml

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const cartWrapper = document.querySelector('.cart-wrapper')
+
 window.addEventListener('click', (event) => {
     if (event.target.dataset.action === 'plus') {
         const counterWrapper = event.target.closest(".counter-wrapper")
@@ -11,5 +13,57 @@ window.addEventListener('click', (event) => {
         if (counter.innerHTML < 1) {
             counter.innerHTML = '1'
         }
+    }
+
+
+
+    if (event.target.hasAttribute('data-cart')) {
+        const card = event.target.closest('.card')
+        
+
+        let cartItem = {
+            id: card.dataset.id,
+            title: card.querySelector('.item-title').innerHTML,
+            itemInBox: card.querySelector('[data-items-in-box]').innerHTML,
+            weight: card.querySelector('.price__weight').innerHTML,
+            price: card.querySelector('.price__currency').innerHTML,
+            img: card.querySelector('.product-img').getAttribute('src'),
+            count: card.querySelector('[data-counter]').innerHTML
+        }
+        
+
+        let itemHtml = `
+        <div class="cart-item" data-id="${cartItem.id}">
+        <div class="cart-item__top">
+          <div class="cart-item__img">
+            <img src="${cartItem.img}" alt="" />
+          </div>
+          <div class="cart-item__desc">
+            <div class="cart-item__title">Филадельфия</div>
+            <div class="cart-item__weight">
+              ${cartItem.itemInBox} / ${cartItem.weight}
+            </div>
+
+            <!-- cart-item__details -->
+            <div class="cart-item__details">
+              <div class="items items--small counter-wrapper">
+                <div class="items__control" data-action="minus">
+                  -
+                </div>
+                <div class="items__current" data-counter="">
+                  ${cartItem.count}
+                </div>
+                <div class="items__control" data-action="plus">+</div>
+              </div>
+
+              <div class="price">
+                <div class="price__currency">
+                  ${cartItem.price}
+                </div>
+              </div>
+            </div>
+        `
+
+        cartWrapper.insertAdjacentHTML('beforeend', itemHtml)
     }
 })
