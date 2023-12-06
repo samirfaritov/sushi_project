f883c00eefae71746623e5bbe4c83bd91c179a08
samirfaritov <samirfaritov6@gmail.com>
2023-12-06 19:34:06 +0500

    korzina

--- js/main.js
+++ js/main.js
@@ -23,84 +23,99 @@ function totalCalc() {
 
 
 
-const cartWrapper = document.querySelector('.cart-wrapper')
+
+
+const cardWrapper = document.querySelector('.cart-wrapper')
 
 window.addEventListener('click', (event) => {
     if (event.target.dataset.action === 'plus') {
-        const counterWrapper = event.target.closest(".counter-wrapper")
+        const counterWrapper = event.target.closest('.counter-wrapper')
         const counter = counterWrapper.querySelector('[data-counter]')
-        counter.innerHTML++
+        counter.innerHTML = ++counter.innerHTML
     }
+
     if (event.target.dataset.action === 'minus') {
-        const counterWrapper = event.target.closest(".counter-wrapper")
+        const counterWrapper = event.target.closest('.counter-wrapper')
         const counter = counterWrapper.querySelector('[data-counter]')
-        counter.innerHTML--
-        if (counter.innerHTML < 1) {
-            counter.innerHTML = '1'
-        } else if (event.target.closest('.cart-wrapper') && parseInt(counter.innerHTML) == 1) {
-          event.target.closest('.cart-item').remove()
-        }
-      }
-      totalCalc()
 
-
-
-    if (event.target.hasAttribute('data-cart')) {
-        const card = event.target.closest('.card')
-        
-
-        let cartItem = {
-            id: card.dataset.id,
-            title: card.querySelector('.item-title').innerHTML,
-            itemInBox: card.querySelector('[data-items-in-box]').innerHTML,
-            weight: card.querySelector('.price__weight').innerHTML,
-            price: card.querySelector('.price__currency').innerHTML,
-            img: card.querySelector('.product-img').getAttribute('src'),
-            count: card.querySelector('[data-counter]').innerHTML
-        }
-
-        const itemCart = cartWrapper.querySelector(`[data-id="${cartItem.id}"]`)
-
-        if (itemCart) {
-          let counterElem = itemCart.querySelector('[data-counter]')
-          counterElem.innerHTML = parseInt(counterElem.innerHTML) + parseInt(cartItem.count)
+        if (parseInt(counter.innerHTML) > 1) {
+            counter.innerHTML = --counter.innerHTML
         } else {
-          let itemHtml = `
-          <div class="cart-item" data-id="${cartItem.id}">
-          <div class="cart-item__top">
+            counter.innerHTML = '1'
+        } 
+    }
+
+    totalCalc()
+
+
+
+    const alert = document.querySelector('.alert-secondary')
+    const orderForm = document.querySelector('#order-form')
+    
+    
+    
+    if (event.target.hasAttribute('data-cart')) {
+      const card = event.target.closest('.card')
+      
+      console.log(card);
+      
+      let cardItem = {
+        id: card.dataset.id,
+        title: card.querySelector('.item-title').innerHTML,
+        itemInBox: card.querySelector('[data-items-in-box]').innerHTML,
+        weight: card.querySelector('.price__weight').innerHTML,
+        price: card.querySelector('.price__currency').innerHTML,
+        img: card.querySelector('.product-img').getAttribute('src'),
+        count: card.querySelector('[data-counter]').innerHTML
+      }
+      
+      let itemHTML = `
+        <div class="cart-item" data-id="${cardItem.id}">
+            <div class="cart-item__top">
             <div class="cart-item__img">
-              <img src="${cartItem.img}" alt="" />
+            <img src="${cardItem.img}" alt="" />
             </div>
             <div class="cart-item__desc">
-              <div class="cart-item__title">Филадельфия</div>
-              <div class="cart-item__weight">
-                ${cartItem.itemInBox} / ${cartItem.weight}
-              </div>
-  
-              <!-- cart-item__details -->
-              <div class="cart-item__details">
-                <div class="items items--small counter-wrapper">
-                  <div class="items__control" data-action="minus">
-                    -
+            <div class="cart-item__title">${cardItem.title}</div>
+            <div class="cart-item__weight">
+                        ${cardItem.itemInBox} / ${cardItem.weight}
+                        </div>
+                        
+                        
+                        
+                        <!-- cart-item__details -->
+                        <div class="cart-item__details">
+                        <div class="items items--small counter-wrapper">
+                          <div class="items__control" data-action="minus">
+                          -
+                          </div>
+                          <div class="items__current" data-counter>
+                            ${cardItem.count}
+                          </div>
+                          <div class="items__control" data-action="plus">+</div>
+                        </div>
+                        
+                        <div class="price">
+                          <div class="price__currency">
+                          ${cardItem.price}
+                          </div>
+                          </div>
+                          </div>
+                          <!-- // cart-item__details -->
+                          </div>
                   </div>
-                  <div class="items__current" data-counter="">
-                    ${cartItem.count}
-                  </div>
-                  <div class="items__control" data-action="plus">+</div>
                 </div>
-  
-                <div class="price">
-                  <div class="price__currency">
-                    ${cartItem.price}
-                  </div>
-                </div>
-              </div>
-          `
-  
-          cartWrapper.insertAdjacentHTML('beforeend', itemHtml)
-          card.querySelector('[data-counter]').innerHTML = '1'
-        }
-        totalCalc()
-        }
+        `
+        
+        cardWrapper.insertAdjacentHTML('beforeend', itemHTML)
+        
+        
+        alert.style.display = 'none'
+        orderForm.style.display = 'block'
+        
+        
+      } 
+
+    console.log(event.target);
 
 })
