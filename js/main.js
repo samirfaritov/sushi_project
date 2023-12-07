4cde3d6c114179dfaabc45ef34f87ecc5d58e229
samirfaritov <samirfaritov6@gmail.com>
2023-12-07 14:58:01 +0500

    tayyor

--- js/main.js
+++ js/main.js
@@ -28,51 +28,46 @@ totalCalc()
 
 const cardWrapper = document.querySelector('.cart-wrapper')
 
-const orderForm = document.querySelector('#order-form')
-const alertt = document.querySelector('.alert-secondary')
-const btn = document.querySelectorAll('.btn-outline-warning')
 
 window.addEventListener('click', (event) => {
-    if (event.target.dataset.action === 'plus') {
-        const counterWrapper = event.target.closest('.counter-wrapper')
-        const counter = counterWrapper.querySelector('[data-counter]')
-        counter.innerHTML = ++counter.innerHTML
-        totalCalc() 
-    }
-
-    if (event.target.dataset.action === 'minus') {
-        const counterWrapper = event.target.closest('.counter-wrapper')
-        const counter = counterWrapper.querySelector('[data-counter]')
-        if (parseInt(counter.innerHTML) > 1) {
-          counter.innerHTML = --counter.innerHTML
+  if (event.target.dataset.action === 'plus') {
+    const counterWrapper = event.target.closest('.counter-wrapper')
+    const counter = counterWrapper.querySelector('[data-counter]')
+    counter.innerHTML = ++counter.innerHTML
+    totalCalc() 
+  }
+  
+  if (event.target.dataset.action === 'minus') {
+    const counterWrapper = event.target.closest('.counter-wrapper')
+    const counter = counterWrapper.querySelector('[data-counter]')
+    if (parseInt(counter.innerHTML) > 1) {
+      counter.innerHTML = --counter.innerHTML
         } else if (event.target.closest('.cart-wrapper') && parseInt(counter.innerHTML) == 1) {
           event.target.closest('.cart-item').remove()
-          orderForm.style.display = 'none'
-          alertt.style.display = 'block'
         }
         totalCalc() 
-    }
-
-
-
-    if (event.target.hasAttribute('data-cart')) {
+      }
+      
+      
+      
+      if (event.target.hasAttribute('data-cart')) {
         // console.log('cart btn');
         const card = event.target.closest('.card')
-
+        
         console.log(card);
-
+        
         let cardItem = {
-            id: card.dataset.id,
-            title: card.querySelector('.item-title').innerHTML,
-            itemInBox: card.querySelector('[data-items-in-box]').innerHTML,
-            weight: card.querySelector('.price__weight').innerHTML,
-            price: card.querySelector('.price__currency').innerHTML,
-            img: card.querySelector('.product-img').getAttribute('src'),
-            count: card.querySelector('[data-counter]').innerHTML
+          id: card.dataset.id,
+          title: card.querySelector('.item-title').innerHTML,
+          itemInBox: card.querySelector('[data-items-in-box]').innerHTML,
+          weight: card.querySelector('.price__weight').innerHTML,
+          price: card.querySelector('.price__currency').innerHTML,
+          img: card.querySelector('.product-img').getAttribute('src'),
+          count: card.querySelector('[data-counter]').innerHTML
         }
-
+        
         // console.log(cardItem);
-
+        
         const itemCart = cardWrapper.querySelector(`[data-id="${cardItem.id}"]`)
         
         
@@ -126,7 +121,10 @@ window.addEventListener('click', (event) => {
       
     })
     
-  
+    
+    const orderForm = document.querySelector('#order-form')
+    const alertt = document.querySelector('.alert-secondary')
+    const btn = document.querySelectorAll('.btn-outline-warning')
     
     for (let i = 0; i < btn.length; i++) {
       btn[i].addEventListener('click', () => {        
